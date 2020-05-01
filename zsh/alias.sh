4ae532020850f506854f9d3e8585e448973b3e4e
YoshikiTsukada <tsukadayyy@gmail.com>
2020-05-01 18:32:32 +0900

    Add alias

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -14,3 +14,18 @@ alias gl='git log'
 
 alias kenkosetup='bundle exec rake carthage:bootstrap && xcodegen && bundle exec pod install'
 alias kenkoopen='open kenko.xcworkspace'
+
+autoCompileTex() {
+  if [ -z "$1" ]; then
+    echo 'Please input file name.'
+  elif [[ -e ./$1.tex ]]; then
+    platex $1.tex
+    dvipdfmx $1.dvi
+    rm $1.aux
+    rm $1.dvi
+    rm $1.log
+    open $1.pdf
+  else
+    echo "$1.tex: No such file, or other errors occurred."
+  fi
+}
